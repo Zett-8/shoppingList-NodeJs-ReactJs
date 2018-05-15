b0292db52f156da75e307cbb182f6c72774165e6
toshiki kamei <toshiki@toshikis-MBPr.local>
2018-05-15 18:08:21 +0900

    fix

--- models/item.js
+++ models/item.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
-  itemname: { type: String, require: true, unique: false },
-  identical: { type: Number, require: true }
+  itemname: { type: String, require: true, unique: false }
   // itemname: { type: String, require: true, unique: true }
 })
 
