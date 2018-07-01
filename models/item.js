20b2d92bc36a26e9a7f1d6a06e6296b1a7e134e1
Zett-8 <kame0008@gmail.com>
2018-07-01 10:56:54 +0900

    gitignore fix

--- models/item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const ItemSchema = new Schema({
-  itemname: { type: String, require: true, unique: false }
-  // itemname: { type: String, require: true, unique: true }
-})
-
-module.exports = mongoose.model('Item', ItemSchema)
