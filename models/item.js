ab865da89200848a8b3bb8bb69dcd1e7ca6bb4c7
Zett-8 <kame0008@gmail.com>
2018-07-01 10:58:23 +0900

    first push

---
+++ models/item.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+  itemname: { type: String, require: true, unique: false }
+  // itemname: { type: String, require: true, unique: true }
+})
+
+module.exports = mongoose.model('Item', ItemSchema)
---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const keys = require('./config/keys')
+const express = require('express')
+// to handle with plain text from form
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json())
+
+const mongoose = require('mongoose')
+mongoose.connect(keys.mongoURI)
+
+const async = require('async')
+const Item = require('./models/item')
+
+app.get('/', (req, res) => {
+  if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    res.sendFile(path.resolve(__dirname, './client/build/index.html'))
+  } else {
+    res.sendFile(path.resolve(__dirname, './client/public/index.html'))
+  }
+})
+
+app.post('/post', (req,res,next) => {
+  console.log(req.body)
+  const items = req.body.itemName
+
+  async.each(items, (eachItem, next) => {
+    const item = new Item()
+    item.itemname = eachItem
+    item.identical = Math.random()
+    item.save(err => {
+      if(err) throw err
+    })
+    next()
+  }, err => {
+    if(err) throw err
+  })
+})
+
+app.get('/get-items', (req, res) => {
+  Item.find({}, (err, users) => {
+    res.send(users)
+  })
+  console.log('"/get-items" returned')
+})
+
+app.post('/item-del', (req, res) => {
+  Item.remove({_id:req.body.id}, (err) => {
+    if(err) console.log(err)
+  })
+  res.send('delete successed')
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('server worked'))
+
