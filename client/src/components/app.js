7f9b1908158455c03ab38aa7517f4168d415b203
Zett-8 <kame0008@gmail.com>
2018-07-01 10:52:38 +0900

    first commit

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from './container'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 export default () => {
   return (
-    <div style={{ margin: '0 auto', width: '500px', textAlign: 'center' }}>
-      <h1 style={{fontSize: '26px'}}>Shopping List</h1>
-      <Container/>
-    </div>
+    <MuiThemeProvider>
+      <div style={{ margin: '0 auto', width: '500px', textAlign: 'center' }}>
+        <h1 style={{fontSize: '26px'}}>Shopping List</h1>
+        <Container/>
+      </div>
+    </MuiThemeProvider>
   )
 }
 
